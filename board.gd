9510962cc74f4ec29c48e23690f4f8101740cccd
AaronWizard <>
2018-02-03 20:09:21 -0400

    Use block scenes directly for falling blocks

--- board.gd
+++ board.gd
@@ -6,31 +6,26 @@ export(Vector2) var board_size = Vector2(10, 20) setget _set_size
 const BORDER_TILE_NAME = "grey"
 const INPUT_TIME = 1.0 / 60.0
 
-var _center_points
+var _block_types = [
+	preload("res://blocks/i.tscn"),
+	preload("res://blocks/j.tscn"),
+	preload("res://blocks/l.tscn"),
+	preload("res://blocks/o.tscn"),
+	preload("res://blocks/s.tscn"),
+	preload("res://blocks/t.tscn"),
+	preload("res://blocks/z.tscn")
+]
 
-var _block_index
-var _block_pos
-var _block_rotation
-
+var _block
 var _game_over
 
 func _ready():
-	$current_tile.tile_set = tile_set
-	$current_tile.cell_size = cell_size
-
+	_block = null
 	_game_over = false
 
 	if not Engine.editor_hint:
 		randomize()
-		_init_center_points()
 		_spawn_block()
-
-func _init_center_points():
-	_center_points = []
-	for b in $blocks.get_children():
-		var size = b.get_used_rect().size
-		var center = Vector2(int(size.x / 2), int(size.y / 2))
-		_center_points.append(center)
 
 func _set_size(value):
 	board_size = value
@@ -52,100 +47,77 @@ func _set_size(value):
 
 func _on_timer_timeout():
 	if not _game_over:
-		if _block_index > -1:
+		if _block != null:
 			_drop_block()
 		else:
 			_spawn_block()
 
-func _spawn_block():
-	_block_index = randi() % $blocks.get_child_count()
-
-	var block = $blocks.get_child(_block_index)
-	var board_middle = int(board_size.x / 2)
-	var block_middle = int(block.get_used_rect().size.x / 2)
-
-	_block_pos = Vector2(board_middle - block_middle + 1, 1)
-	_block_rotation = 0
-
-	_draw_current_block()
-	if not _is_block_space_empty(_block_pos, _block_rotation):
-		_end_game()
-
-func _get_block_tiles(pos = _block_pos, rot = _block_rotation):
-	var result = {}
-
-	var block = $blocks.get_child(_block_index)
-	var center = _center_points[_block_index]
-
-	for tile in block.get_used_cells():
-		var real_tile = (tile - center).rotated(rot) + center + pos
-		result[tile] = real_tile
-	return result
-
-func _clear_block():
-	var tiles = _get_block_tiles()
-	for t in tiles.values():
-		$current_tile.set_cellv(t, -1)
-
-func _draw_block(tilemap):
-	var block = $blocks.get_child(_block_index)
-	var tiles = _get_block_tiles()
-
-	for t in tiles:
-		var real_tile = tiles[t]
-		tilemap.set_cellv(real_tile, block.get_cellv(t))
-
-func _draw_current_block():
-	_draw_block($current_tile)
-
-func _end_block():
-	_draw_block(self)
-	$current_tile.clear()
-	_block_index = -1
-
-func _drop_block():
-	if not Input.is_action_pressed("move_down"):
-		_move_block(Vector2(0, 1), 0)
-
-func _move_block(pos, rot):
-	if _is_block_space_empty(_block_pos + pos, _block_rotation + rot):
-		_clear_block()
-		_block_pos += pos
-		_block_rotation += rot
-		_draw_current_block()
-
-		if not _is_block_space_empty(_block_pos + Vector2(0, 1), 0):
-			_end_block()
-
 func _on_input_timer_timeout():
-	if _block_index > -1:
+	if _block != null:
 		var move = Vector2()
 		var rotate = 0
-	
+
 		if Input.is_action_pressed("move_left"):
 			move.x -= 1
 		if Input.is_action_pressed("move_right"):
 			move.x += 1
 		if Input.is_action_pressed("move_down"):
 			move.y += 1
-	
+
 		if Input.is_action_pressed("rotate_ccw"):
 			rotate -= 1
 		if Input.is_action_pressed("rotate_cw"):
 			rotate += 1
-	
-		_move_block(move, 0)
+
+		_move_block(move, rotate)
+
+func _spawn_block():
+	var index = randi() % _block_types.size()
+	_block = _block_types[index].instance()
+	add_child(_block)
+
+	var block_rect = _block.get_rect()
+
+	var board_middle = int(board_size.x / 2)
+	var block_middle = int(block_rect.size.x / 2)
+
+	var block_pos = Vector2(board_middle - block_middle + 1, 1)
+	_block.block_position = block_pos
+
+	if not _is_block_space_empty(block_pos, 0):
+		_end_game()
+
+func _drop_block():
+	if not Input.is_action_pressed("move_down"):
+		_move_block(Vector2(0, 1), 0)
+
+	if not _is_block_space_empty(_block.block_position + Vector2(0, 1),
+			_block.block_rotation):
+		_end_block()
+
+func _move_block(pos, rot):
+	var new_pos = _block.block_position + pos
+	var new_rot = _block.block_rotation + rot
+
+	if _is_block_space_empty(new_pos, new_rot):
+		_block.block_position = new_pos
+		_block.block_rotation = new_rot
 
 func _is_block_space_empty(pos, rot):
 	var result = true
-
-	var tiles = _get_block_tiles(pos, rot)
-	for t in tiles.values():
+	for t in _block.get_tiles(pos, rot):
 		if get_cellv(t) != -1:
 			result = false
 			break
-
 	return result
+
+func _end_block():
+	var tiles = _block.get_tiles()
+	for t in tiles:
+		set_cellv(t + _block.block_position, _block.get_tile_type(t))
+
+	_block.queue_free()
+	_block = null
 
 func _end_game():
 	_game_over = true
